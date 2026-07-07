fd021f4516d459664c77b4910ed6c41c32c2dd5a
suji-gelam <sujithagelam@gmail.com>
2026-07-08 05:08:01 +0530

    fixed correct spelling

--- src/suby/pages/LandingPage.jsx
+++ src/suby/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import Topbar from "../components/Topbar";
 import ItemsDisplay from "../components/ItemsDisplay";
 import Chains from "../components/Chains";
 import FirmCollections from "../components/FirmCollections";
-import ProductMenu from "../components/productMenu";
+import ProductMenu from "../components/ProductMenu";
 
 const LandingPage = () => {
   return (
